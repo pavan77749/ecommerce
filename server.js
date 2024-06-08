a3ac531c66bb4f945a824a4d67e436da246d612b
harsh <harshbhai90@gmail.com>
2024-06-08 13:53:41 +0530

    created the database, userSchema

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 import express from "express"
 import colors from "colors"
 import dotenv from "dotenv"
+import morgan from "morgan"
+import connectDB from "./config/db.js"
 
 //configure env
-dotenv.config({path:''})
+dotenv.config()
+
+//connect database config
+connectDB();
+
 
 //rest object
 const app = express()
+
+// middle wares
+app.use(express.json())
+app.use(morgan('dev'))
 
 //rest api
 app.get('/',(req,res)=>{
